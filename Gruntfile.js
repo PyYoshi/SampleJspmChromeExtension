a514050f485f381238d5d86d8d9a1747cfe60593
PyYoshi <myoshi321go@gmail.com>
2015-10-16 15:49:15 +0900

    add release task for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,6 @@
+var Builder = require('systemjs-builder');
+var Rsvp = require('rsvp');
+
 module.exports = function(grunt) {
     require('jit-grunt')(grunt, {
         replace: 'grunt-text-replace'
@@ -48,6 +51,25 @@ module.exports = function(grunt) {
                     '**/main.js'
                 ],
                 dest: 'compiled/'
+            },
+            release: {
+                expand: true,
+                cwd: 'dist/',
+                src: '**/*',
+                dest: '<%= meta.releaseDirectory %>'
+            },
+            dist: {
+                expand: true,
+                cwd: 'src/',
+                src: [
+                    '_locales/**',
+                    'css/**',
+                    'img/**',
+                    'manifest.json',
+                    'options.html',
+                    'popup.html'
+                ],
+                dest: 'dist/'
             }
         },
         watch: {
@@ -160,9 +182,133 @@ module.exports = function(grunt) {
                     urls: ['http://localhost:4044/test.html']
                 }
             }
+        },
+
+        htmlmin: {
+            options: {
+                removeComments: true,
+                collapseWhitespace: true,
+                collapseBooleanAttributes: true,
+                removeAttributeQuotes: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                removeOptionalTags: true
+            },
+            dist: {
+                files: {
+                    'dist/options.html': 'dist/options.html',
+                    'dist/popup.html': 'dist/popup.html'
+                }
+            }
+        },
+        imagemin: {
+            files: {
+                expand: true,
+                cwd: 'dist/img',
+                src: ['**/*.{png,jpg,gif}'],
+                dest: 'dist/img/'
+            }
+        },
+        replace: {
+            htmlBundle: {
+                src: ['dist/*.html'],
+                overwrite: true,
+                replacements: [
+                    {
+                        from: '<script src=\'js/lib/jspm_packages/system.js\'></script>',
+                        to: ''
+                    },
+                    {
+                        from: '<script src=\'js/lib/jspm.config.js\'></script>',
+                        to: ''
+                    }
+                ]
+            }
+        },
+        clean: {
+            compiledFile: {
+                expand: true,
+                cwd: 'compiled',
+                src: ''
+            }
         }
     });
 
     grunt.registerTask('compile', ['copy:compiled', 'babel:compiled', 'watch']);
     grunt.registerTask('test', ['jshint', 'jscs', 'connect', 'mocha']);
+
+    grunt.registerTask('buildDist', function() {
+        grunt.task.run('copy:dist');
+
+        var backgroundBuilder = new Builder('src/', 'src/js/lib/jspm.config.js');
+        var contentScriptsBuilder = new Builder('src/', 'src/js/lib/jspm.config.js');
+        var optionsBuilder = new Builder('src/', 'src/js/lib/jspm.config.js');
+        var popupBuilder = new Builder('src/', 'src/js/lib/jspm.config.js');
+
+        var done = this.async();
+        var options = {
+            runtime: false,
+            sourceMaps: false,
+            minify: true
+        };
+
+        Rsvp.Promise.all([
+            backgroundBuilder.buildStatic('js/background/app.js', 'dist/js/background/main.js', options),
+            contentScriptsBuilder.buildStatic('js/content_scripts/say_hello/app.js', 'dist/js/content_scripts/say_hello/main.js', options),
+            optionsBuilder.buildStatic('js/options/app.js', 'dist/js/options/main.js', options),
+            popupBuilder.buildStatic('js/popup/app.js', 'dist/js/popup/main.js', options),
+        ]).then(function() {
+            grunt.task.run('replace:htmlBundle');
+            done();
+        }).catch(function(error) {
+            console.log('buildStatic error:', error);
+            done();
+        });
+    });
+
+    grunt.registerTask('buildManifestJSON', function(buildFlag){
+        var isRelease = false;
+        if (buildFlag === 'release') {
+            isRelease = true;
+        }
+
+        // load json
+        var manifestJSON = grunt.file.readJSON('dist/manifest.json');
+
+        // background script
+        manifestJSON['background']['scripts'] = ['js/background/main.js'];
+
+        // content scripts
+        var contentScripts = manifestJSON['content_scripts'];
+        for(var i=0; i<contentScripts.length; i++) {
+            if ('js' in contentScripts[i]) {
+                for(var j=0; j<contentScripts[i].js.length; j++) {
+                    if (contentScripts[i].js[j].includes('/main.js')) {
+                        contentScripts[i].js = [contentScripts[i].js[j]];
+                        break;
+                    }
+                }
+            }
+        }
+
+        // key
+        if (isRelease) {
+            delete manifestJSON['key'];
+        }
+
+        // save json
+        var strJSON = JSON.stringify(manifestJSON, null, '  ');
+        grunt.file.write('dist/manifest.json', strJSON);
+    });
+
+    grunt.registerTask('build', function(buildFlag) {
+        var isRelease = buildFlag === 'release';
+
+        grunt.task.run('buildDist');
+        grunt.task.run('buildManifestJSON:'+buildFlag);
+        grunt.task.run('imagemin', 'htmlmin');
+    });
 };
